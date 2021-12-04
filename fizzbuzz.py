92b32e4fdff5e2277eb39f32e2e73636fdee003d
Martin Kirchgessner <mkirchgessner@luxia.fr>
2021-12-04 17:46:07 +0100

    note on \n

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,6 +6,7 @@ on battery:
  * naive implem peaks around 10Mb/s
  * modulo 3 and 5 only once and sys.stdout.write("Fizz"/"Buzz") peaks around 6Mb/s
  * same but in py3 gets back to 10mb/s
+ * and integrating the \n to all outputted strings (to avoid a call to `write`) peaks at 13.5Mb/s
  * using sys.stdout.buffer.write(b'Buzz') and sys.stdout.buffer.write(b'%d' % i) peaks around 9.5Mb/s
  * itertools.cycle over a the fizzbuzz cycle and sys.stdout.buffer.write(b'%d' % i) peaks at 11Mb/s
  * integrating \n to each sys.stdout.buffer.write peaks at 15Mb/s !!
