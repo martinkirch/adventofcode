67845481f236be9326619368b651eefd00eee382
Martin Kirchgessner <mkirchgessner@luxia.fr>
2021-12-04 16:54:15 +0100

    py3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,9 +5,13 @@ as from https://codegolf.stackexchange.com/questions/215216/high-throughput-fizz
 on battery:
  * naive implem peaks around 10Mb/s
  * modulo 3 and 5 only once and sys.stdout.write("Fizz"/"Buzz") peaks around 6Mb/s
+ * same but in py3 gets back to 10mb/s
+
 """
 
 import sys
+
+# 20000000 does 153MiB
 
 for i in range(20000000):
     flag = True
