5f907d0c41abc50983b80cd06759a1d89c6e780a
Martin Kirchgessner <martin.kirch@gmail.com>
2021-12-17 22:05:51 +0100

    day11 part 1 and 2

--- day11.py
+++ day11.py
@@ -1,3 +1,9 @@
+smol_input = """11111
+19991
+19191
+19991
+11111"""
+
 test_input = """5483143223
 2745854711
 5264556173
@@ -20,9 +26,77 @@ challenge_input = """2478668324
 1366237577
 3554687226"""
 
-INPUT = test_input
+INPUT = challenge_input
 
 map = [[int(c) for c in line] for line in INPUT.split()]
+width = len(map)
+jmax = width - 1
+nb_octopuses = width * width
+
+def step_start():
+    contain_10 = False
+    for i in range(width):
+        for j in range(width):
+            map[i][j] += 1
+            contain_10 = contain_10 or map[i][j] == 10
+    return contain_10
+
+def flash_maybe(i, j):
+    if map[i][j] in (0, 10):
+        return False #flashed already, or is waiting to flash
+    else:
+        map[i][j] += 1
+        return map[i][j] == 10
+
+def spread(i, j):
+    flash_found = False
+    if i > 0:
+        flash_found = flash_maybe(i-1, j) or flash_found
+        if j > 0:
+            flash_found = flash_maybe(i-1, j-1) or flash_found
+        if j < jmax:
+            flash_found = flash_maybe(i-1, j+1) or flash_found
+    if i < jmax:
+        flash_found = flash_maybe(i+1, j) or flash_found
+        if j > 0:
+            flash_found = flash_maybe(i+1, j-1) or flash_found
+        if j < jmax:
+            flash_found = flash_maybe(i+1, j+1) or flash_found
+    if j > 0:
+        flash_found = flash_maybe(i, j-1) or flash_found
+    if j < jmax:
+        flash_found = flash_maybe(i, j+1) or flash_found
+    return flash_found
+
+
+def flash():
+    found = 0
+    flashes_remaining = False
+    for i in range(width):
+        for j in range(width):
+            if map[i][j] == 10:
+                found += 1
+                flashes_remaining = spread(i, j) or flashes_remaining
+                map[i][j] = 0
+    return (flashes_remaining, found)
 
 def step():
-    
+    flashes = 0
+    flashes_remaining = step_start()
+    while flashes_remaining:
+        (flashes_remaining, found) = flash()
+        flashes += found
+    return flashes
+
+total = 0
+for i in range(1000):
+    step_flashes = step()
+    if step_flashes == nb_octopuses:
+        print(f"All flashing at step {i+1}")
+        break
+    total += step_flashes
+
+for line in map:
+    print(line)
+
+print(f"\ntotal {total} flashes")
