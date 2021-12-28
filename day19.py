193f158e6767f02d608dab5ed033ecadcd1249d7
Martin Kirchgessner <mkirchgessner@luxia.fr>
2021-12-28 20:28:22 +0100

    still incorrect, but faster

--- day19.py
+++ day19.py
@@ -2,8 +2,8 @@
 Day 19: Beacon Scanner
 """
 from __future__ import annotations
-from typing import Optional
 from dataclasses import dataclass
+
 @dataclass
 class Point():
     x:int
@@ -50,48 +50,30 @@ def compute_sorted_distances(l:list[Point]):
     for i in range(len(l) - 1):
         for j in range(i + 1, len(l)):
             distances[i].append(distances[j][i])
-    for i in range(len(l)):
-        distances[i].sort()
-        distances[i].pop(0)
     return distances
 
 distances = [compute_sorted_distances(scanner) for scanner in INPUT]
+distances_sets = []
+for scanner_id in range(len(distances)):
+    scanner_list = []
+    for point_id in range(len(distances[scanner_id])):
+        point_set = set(distances[scanner_id][point_id])
+        point_set.remove(0)
+        scanner_list.append(point_set)
+    distances_sets.append(scanner_list)
 
-def find_overlaps(a:list[int], b:list[int]) -> Optional[int]:
-    """
-    Assumes a and b are sorted and of same length
-    Returns:
-        number of overlapping distances
-    """
-    i = 0
-    j = 0
-    nb_matching = 0
-    max_i = len(a)
-    max_j = len(b)
-    while i < max_i and j < max_j:
-        if a[i] == b[j]:
-            i += 1
-            j += 1
-            nb_matching += 1
-        elif a[i] > b[j]:
-            j += 1
-        else:
-            i += 1
-    return nb_matching
-
-def match_distances(scanner1:list, scanner2:list):
+def match_distances(scanner1:int, scanner2:int):
     """
     we search for common beacons seen from scanner1 and scanner2 - but as seen
     from one point it means we try to find 11 similar distances
     """
     matching_points = {}
     matched = set()
-    for point_i in range(len(scanner1)):
-        for point_j in range(len(scanner2)):
+    for point_i in range(len(distances_sets[scanner1])):
+        for point_j in range(len(distances_sets[scanner2])):
             if point_j in matched:
                 continue
-            matches = find_overlaps(scanner1[point_i], scanner2[point_j])
-            if matches:
+            if not distances_sets[scanner1][point_i].isdisjoint(distances_sets[scanner2][point_j]):
                 matching_points[point_i] = point_j
                 matched.add(point_j)
                 break
@@ -102,7 +84,7 @@ points_count = 0
 for scanner_id in range(len(distances)):
     points_in_scanner_id_or_more = set([i for i in range(len(distances[scanner_id]))])
     for other_scanner_id in range(scanner_id):
-        overlaps = match_distances(distances[scanner_id], distances[other_scanner_id])
+        overlaps = match_distances(scanner_id, other_scanner_id)
         if len(overlaps) >= 12:
             print(f"Scanners {scanner_id} and {other_scanner_id} overlap ({len(overlaps)} points)")
         #     for point_i, point_j in overlaps.items():
