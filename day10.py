1c60818ef3b67ad3b32d6d6db20462ed06af88d4
Martin Kirchgessner <martin.kirch@gmail.com>
2022-12-11 22:15:44 +0100

    day10 part 2, day11 part 1

--- day10.py
+++ day10.py
@@ -3,24 +3,39 @@ lines = open("day10_input.txt").readlines()
 x = 1
 cycle = 0
 strength = 0
+screen = []
+for i in range(6):
+    screen.append(['.'] * 40)
 
 def update_strength():
     global strength, cycle, x
     if (cycle % 40) == 20:
         strength += cycle * x
 
+def draw():
+    global cycle, x, screen
+    row = cycle // 40
+    pixel_pos = cycle % 40
+    if x == pixel_pos or (x-1) == pixel_pos or (x+1) == pixel_pos:
+        screen[row][pixel_pos] = '#'
+
 for line in lines:
     if line.startswith("noop"):
+        draw()
         cycle += 1
         update_strength()
     else:
+        draw()
         val = int(line.split()[1])
         cycle += 1
         update_strength()
+        draw()
         cycle += 1
         update_strength()
         x += val
 
 print(strength)
-        
+
+for l in screen:
+    print(''.join(l))
     
---
+++ day11.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+lines = open("day11_input.txt").readlines()
+
+class Monkey:
+    def __init__(self, lines:list[str]):
+        self.inspected = 0
+        self.index = int(lines[0][7:-2])
+        self.items = [int(i) for i in lines[1][18:-1].split(",")]
+        self.operation = lines[2][19:-1]
+        self.test_divisible = int(lines[3][21:-1])
+        self.dest_if_test = int(lines[4].split()[-1])
+        self.dest_if_not_test = int(lines[5].split()[-1])
+    
+    def __str__(self):
+        return f"""Monkey {self.index}:
+        Items: {self.items}
+        Operation: {self.operation}
+        Test: divisible by {self.test_divisible}
+           if true: give to monkey {self.dest_if_test}
+           if false: give to monkey {self.dest_if_not_test}
+        """
+    
+    def turn(self):
+        global monkeys
+        while self.items:
+            self.inspected += 1
+            old = self.items.pop(0)
+            worry = eval(self.operation)
+            worry = worry // 3
+            if (worry % self.test_divisible) == 0:
+                monkeys[self.dest_if_test].items.append(worry)
+            else:
+                monkeys[self.dest_if_not_test].items.append(worry)
+
+monkeys:list[Monkey] = list()
+i=0
+while i < len(lines):
+    monkeys.append(Monkey(lines[i:i+7]))
+    i += 7
+
+for round in range(20):
+    for monkey in monkeys:
+        monkey.turn()
+    print(f"round {round}")
+    for monkey in monkeys:
+        print(f"Monkey {monkey.index} holds {monkey.items}")
+
+for monkey in monkeys:
+    print(f"Monkey {monkey.index} inspected items {monkey.inspected} times")
+
+inspections = [m.inspected for m in monkeys]
+inspections.sort(reverse=True)
+print(inspections[0]*inspections[1])
