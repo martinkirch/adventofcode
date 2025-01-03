22c84acbcabdd874e185fe1406913facb6c45683
Martin Kirchgessner <martin.kirch@gmail.com>
2025-01-03 16:58:21 +0100

    handle "run 1bis", prepare templates

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ pub mod day1;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let day = args[1].parse::<u32>().unwrap_or_else(|_| panic!("Invalid day number"));
+    if args.len() < 1 {
+        panic!("give the day number ! 1 or 1bis");
+    }
+    let day = args[1].as_str();
     let file = format!("input{}.txt", day);
     let input = fs::read_to_string(&file).unwrap_or_else(|_| panic!("Could not read input file: {}", file));
     match day {
-        1 => println!("{}", day1(&input)),
+        "1" => println!("{}", day1(&input)),
         _ => panic!("Invalid day number"),
     }
 }
