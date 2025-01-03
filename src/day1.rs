4ed8c442d485bd09feff868623fdd0d9896a55b7
Martin Kirchgessner <martin.kirch@gmail.com>
2025-01-03 16:47:15 +0100

    day1_1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,20 @@
 pub fn compute(input:&String) -> String {
-    return "123".to_string();
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    for line in input.lines() {
+        let mut splitted = line.split_whitespace();
+        let a = splitted.next().unwrap().parse::<i32>().unwrap();
+        let b = splitted.next().unwrap().parse::<i32>().unwrap();
+        left.push(a);
+        right.push(b);
+    }
+    left.sort();
+    right.sort();
+    let mut counter = 0;
+    for i in 0..left.len() {
+        counter += (left[i] - right[i]).abs();
+    }
+    return counter.to_string();
 }
 
 #[cfg(test)]
@@ -8,7 +23,12 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = compute(&"lol".to_string());
-        assert_eq!(result, "123");
+        let result = compute(&"3   4
+4   3
+2   5
+1   3
+3   9
+3   3".to_string());
+        assert_eq!(result, "11");
     }
 }
