414ded8020c66a374bd991aa45c7f61d8610105e
Martin Kirchgessner <martin.kirch@gmail.com>
2025-12-06 13:38:59 +0100

    day6

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,26 @@
 pub fn compute(input:&String) -> String {
-    return "4277556".to_string();
+    let mut numbers = Vec::new();
+    let mut operations = "";
+    let mut grand_total:u64 = 0;
+    for line in input.lines() {
+        if line.contains('+') {
+            operations = line;
+        } else {
+            let parsed = Vec::from_iter(line.split_ascii_whitespace().map(|n| n.parse::<u64>().unwrap()));
+            numbers.push(parsed);
+        }
+    }
+    let mut i = 0;
+    for c in operations.split_ascii_whitespace() {
+        let column = numbers.iter().clone().map(|l| l[i]);
+        grand_total += match c {
+            "+" => column.sum::<u64>(),
+            "*" => column.product::<u64>(),
+            _ => panic!("unknown op: {}", c),
+        };
+        i += 1;
+    }
+    return grand_total.to_string();
 }
 
 #[cfg(test)]
